4d5597de55e7afcf719c5f2398557ab87e18fbe2
joshua beeson <5590@holbertonstudents.com>
2023-03-28 16:44:12 -0700

    dot sql adjustments

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,4 +1,4 @@
--- Creates hbnb_dev_db && hbnb_dev
+-- Create hbnb_dev_db && hbnb_dev
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost'
 IDENTIFIED BY 'hbnb_dev_pwd';
